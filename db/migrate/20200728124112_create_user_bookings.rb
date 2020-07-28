5d4fb72bcd98966285be766c34ec88f845888a96
EC2 Default User <ec2-user@ip-172-31-41-209.ap-northeast-1.compute.internal>
2020-07-28 12:55:29 +0000

    install of devise

--- db/migrate/20200728124112_create_user_bookings.rb
+++ db/migrate/20200728124112_create_user_bookings.rb
@@ -1,7 +1,8 @@
 class CreateUserBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :user_bookings do |t|
-
+      t.integer :user_id
+      t.integer :booking_id
       t.timestamps
     end
   end
