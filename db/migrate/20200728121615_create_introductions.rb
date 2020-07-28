d333849c8ea94f3ade1254fb2ffc93e83cb1d222
EC2 Default User <ec2-user@ip-172-31-41-209.ap-northeast-1.compute.internal>
2020-07-28 12:18:40 +0000

    creation of introduction table

--- db/migrate/20200728121615_create_introductions.rb
+++ db/migrate/20200728121615_create_introductions.rb
@@ -1,7 +1,9 @@
 class CreateIntroductions < ActiveRecord::Migration[5.2]
   def change
     create_table :introductions do |t|
-
+      t.string :name
+      t.text :introduction
+      t.text :image
       t.timestamps
     end
   end
