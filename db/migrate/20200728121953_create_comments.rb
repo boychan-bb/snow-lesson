da9612dce85eb4dac137c1fc402952e0a6fcfc2b
EC2 Default User <ec2-user@ip-172-31-41-209.ap-northeast-1.compute.internal>
2020-07-28 12:22:33 +0000

    creation of comment table

--- db/migrate/20200728121953_create_comments.rb
+++ db/migrate/20200728121953_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.text :comment
+      t.integer :user_id
+      t.integer :instructor_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_121615) do
+ActiveRecord::Schema.define(version: 2020_07_28_121953) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "instructor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "introductions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
