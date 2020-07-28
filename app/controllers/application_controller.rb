3cc751c7a2b47d85a669d7e8aed420b308827b8b
EC2 Default User <ec2-user@ip-172-31-41-209.ap-northeast-1.compute.internal>
2020-07-28 13:17:51 +0000

    add application_controller configure permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :divise_controller? #devise用のコントローラの場合にtrueとなる
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
