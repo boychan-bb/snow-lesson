cbc2c001b48359067fd85136d7af251a7b1e9908
EC2 Default User <ec2-user@ip-172-31-41-209.ap-northeast-1.compute.internal>
2020-07-31 22:09:34 +0000

    creation of top model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_130159) do
+ActiveRecord::Schema.define(version: 2020_07_31_220810) do
 
   create_table "bookings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "lesson_date"
@@ -54,6 +54,11 @@ ActiveRecord::Schema.define(version: 2020_07_28_130159) do
     t.string "name"
     t.text "introduction"
     t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
